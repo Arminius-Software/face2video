caba02a63a16b93dca3fc5609729c9f91636a5c4
Timo <75044824+Arminius-Software@users.noreply.github.com>
2025-04-26 14:47:54 +0200

    set fps explicitly to avoid crash on linux os

--- copy_sound_from_video.py
+++ copy_sound_from_video.py
@@ -4,14 +4,16 @@ import os
 
 
 def add_sound(video_sound_path, video_no_sound_path):
-
+    
     video_with_sound = VideoFileClip(video_sound_path)
-
     video_without_sound = VideoFileClip(video_no_sound_path)
 
     video_without_sound = video_without_sound.set_audio(video_with_sound.audio)
 
+    fps = video_with_sound.fps or 30
     current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    video_without_sound.write_videofile("finished_videos/final_{}.mp4".format(current_datetime), codec='libx264')
+    output_path = "finished_videos/final_{}.mp4".format(current_datetime)
+
+    video_without_sound.write_videofile(output_path, codec='libx264', fps=fps)
 
     os.remove(video_no_sound_path)
